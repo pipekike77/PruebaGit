0cb267ad5074eca83d81a72cd4607afc14081917
unknown <Laboratorioi2t@D004E46.icesi.edu.co>
2015-08-09 23:03:31 -0500

    Initial commit

---
+++ app/src/androidTest/java/dengue/fvl/i2t/pruebagit/ApplicationTest.java
@@ -0,0 +1,13 @@
+package dengue.fvl.i2t.pruebagit;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
